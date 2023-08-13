d0651cd031242c761e544a4b10b7596b47edc150
EgorKholod <veh@kvartel.com>
2023-08-13 17:40:37 +0300

    initial_commit

---
+++ app/src/main/java/com/example/brainstorm/Presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.brainstorm.Presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.brainstorm.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
